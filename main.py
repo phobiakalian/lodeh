7d96356f5f5be62bdf445b649a467cae124e620f
phobiakalian <phobiakalian@gmail.com>
2025-05-13 23:40:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 # Konfigurasi akunmu
 api_id = 6973446  # Ganti dengan API ID kamu
 api_hash = "d3a6dbd3e466159f7170f6af7fb35ac1"  # Ganti dengan API Hash kamu
-session = "BQBqaAYAeEDq0DsANq8B7-Wt_ddQe0hG4ee859GS1Svws8CeTWXrebXeekUTPy1rG3xRYmYWPrb2jfsBu0AViEyGtKtA7cDLGaw4st-uxfnK35CPH1X680sjIfhG2YlLSuARV2fg2s4LyyHE_mXsZDt2F0V10u7HDPNzt-8-eYko69hNWbmxGt6ZJXgEaKoS8PVCYSuU9_Qk9L_qjl0q2CVt1vWmSefeLDMb6sfadhFG-UD7z7PY8jwa2pahMhDXYzBIekILQEqX0B5e1KUBet-aHKTUyycD9njTplcEZ6nFQV4LdjwK2FZjP6a205gjccw3TqMHI6gEidPMLFzhDRPr4OCBYgAAAAB1B3HOAA"  # Ganti dengan session string kamu
+session = "BQBqaAYAlRD7Is4yeAS34BC7tMiaPXfqJhhSgodk3cs4mz6n3bR1AfCsixwXlpmCbk1SSDYkXTwRCOG0EGHxge61v39OdazqjHG9SJ2shlaqc-faFIwn7_8cIQnrbPxCURUFEY65NjGdXR1nGWQ_f8o7uqS4nLQ9T0PLi8-uhP1Qb94qMnqpA9zMGl-TYDYspS4I2MLmIIjLDiXv4y4DaAtEMhry3B00BY36pvkbCtL9hd9bwYx1OknJAf_IqN-bLnEIucTIkfrG-2a3_LhCgXzJUonkKwUJQ-9BJT-YYDbNy5BRkgNDlsc4pl6ov2vn2V7PdygTL-axrMLRT_dP7Aj2Dy4_GwAAAAB1B3HOAA"
 
 # ID atau username bot anonymous (contoh)
 bot_username = "chatbot"
@@ -23,10 +23,6 @@ kata_kata = [
     "sifa punya grup,join ya klik stiker diatas",
     "temenin sifa ngobrol,klik stiker diatas",
 ]
-stk = [
-    "CAACAgUAAxkDAAEFHwtoDnmppvmFK4XHPt2uD429wlzGyAACOxUAAgJIeFTLtdY1smEGex4E",
-    "CAACAgUAAxkDAAEFHvtoDnfTve9yrixL5v76zLW61hg6kAACMRkAApGMcVR642NGfTJX-R4E",
-]
 @app.on_message(filters.bot & filters.chat(bot_username))
 async def handle_messages(client, message):
     global chatting, current_chat
@@ -40,15 +36,13 @@ async def handle_messages(client, message):
     if "/next" in message.text.lower():
         chatting = True
         current_chat = message.chat.id
-        
-        stikernya = random.choice(stk)
         katanya = random.choice(kata_kata)
 
         # Kirim stiker
         await asyncio.sleep(2)  # Biar natural
         await client.send_message(bot_username, "ce")
         await asyncio.sleep(1)
-        await client.send_sticker(bot_username, stikernya)
+        await client.send_sticker(bot_username, "CAACAgUAAyEGAASTIE8vAAIBz2gjb5Yq3kb7-1eQ2HEbxwJd_kjlAAI7FQACAkh4VMu11jWyYQZ7HgQ")
         await client.send_message(bot_username, katanya)
 
         # Tunggu 10 detik, kalau belum di-skip, kita auto skip
